89bb996bebb3fb15686e2f945bd46bba363d0c93
Nikolay <nickwebdev89@gmail.com>
2024-05-17 17:58:07 +0300

    notice and it's logic ready

---
+++ js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//notice 
+const noticeEl = document.querySelector(".notice");
+
+if (noticeEl) {
+  const noticeCloseEl = noticeEl.querySelector(".notice__close");
+  noticeCloseEl.addEventListener("click", () => {
+    noticeEl.classList.add("notice--hidden");
+  });
+}
