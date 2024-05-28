153887dac658196b057c7d3cc745b806f2b52134
Nikolay <nickwebdev89@gmail.com>
2024-05-28 17:35:33 +0300

    cart page ready

--- js/main.js
+++ js/main.js
@@ -12,61 +12,64 @@ if (noticeEl) {
 
 //stepper
 const stepperEl = document.querySelector(".stepper");
+const stepperEls = document.querySelectorAll(".stepper");
 
-if (stepperEl) {
-  const stepperInputEl = stepperEl.querySelector(".stepper__input");
-  const stepperBtnMinusEl = stepperEl.querySelector(".stepper__btn--decrease");
-  const stepperBtnPlusEl = stepperEl.querySelector(".stepper__btn--increase");
+if (stepperEls) {
+  stepperEls.forEach((stepperEl) => {
+    const stepperInputEl = stepperEl.querySelector(".stepper__input");
+    const stepperBtnMinusEl = stepperEl.querySelector(".stepper__btn--decrease");
+    const stepperBtnPlusEl = stepperEl.querySelector(".stepper__btn--increase");
 
-  const stepperMin = Number(stepperInputEl.getAttribute("min"));
-  const stepperMax = Number(stepperInputEl.getAttribute("max"));
+    const stepperMin = Number(stepperInputEl.getAttribute("min"));
+    const stepperMax = Number(stepperInputEl.getAttribute("max"));
 
-  let count = Number(stepperInputEl.value);
+    let count = Number(stepperInputEl.value);
 
-  stepperInputEl.addEventListener("change", () => {
-    stepperBtnPlusEl.classList.remove("stepper__btn--disabled");
-    stepperBtnMinusEl.classList.remove("stepper__btn--disabled");
+    stepperInputEl.addEventListener("change", () => {
+      stepperBtnPlusEl.classList.remove("stepper__btn--disabled");
+      stepperBtnMinusEl.classList.remove("stepper__btn--disabled");
 
-    if (stepperInputEl.value < stepperMin) {
-      stepperInputEl.value = stepperMin;
-      stepperBtnPlusEl.classList.add("stepper__btn--disabled");
-    }
+      if (stepperInputEl.value < stepperMin) {
+        stepperInputEl.value = stepperMin;
+        stepperBtnPlusEl.classList.add("stepper__btn--disabled");
+      }
 
-    if (stepperInputEl.value > stepperMax) {
-      stepperInputEl.value = stepperMax;
-      stepperBtnPlusEl.classList.add("stepper__btn--disabled");
-    }
-  });
+      if (stepperInputEl.value > stepperMax) {
+        stepperInputEl.value = stepperMax;
+        stepperBtnPlusEl.classList.add("stepper__btn--disabled");
+      }
+    });
 
-  stepperBtnPlusEl.addEventListener("click", () => {
-    count = Number(stepperInputEl.value);
+    stepperBtnPlusEl.addEventListener("click", () => {
+      count = Number(stepperInputEl.value);
 
-    stepperBtnPlusEl.classList.remove("stepper__btn--disabled");
-    stepperBtnMinusEl.classList.remove("stepper__btn--disabled");
+      stepperBtnPlusEl.classList.remove("stepper__btn--disabled");
+      stepperBtnMinusEl.classList.remove("stepper__btn--disabled");
 
-    if (count < stepperMax) {
-      count++;
-      stepperInputEl.value = count;
-    }
+      if (count < stepperMax) {
+        count++;
+        stepperInputEl.value = count;
+      }
 
-    if (count === stepperMax) {
-      stepperBtnPlusEl.classList.add("stepper__btn--disabled");
-    }
-  });
+      if (count === stepperMax) {
+        stepperBtnPlusEl.classList.add("stepper__btn--disabled");
+      }
+    });
 
-  stepperBtnMinusEl.addEventListener("click", () => {
-    count = Number(stepperInputEl.value);
+    stepperBtnMinusEl.addEventListener("click", () => {
+      count = Number(stepperInputEl.value);
 
-    stepperBtnMinusEl.classList.remove("stepper__btn--disabled");
-    stepperBtnPlusEl.classList.remove("stepper__btn--disabled");
+      stepperBtnMinusEl.classList.remove("stepper__btn--disabled");
+      stepperBtnPlusEl.classList.remove("stepper__btn--disabled");
 
-    if (count > stepperMin) {
-      count--;
-      stepperInputEl.value = count;
-    }
+      if (count > stepperMin) {
+        count--;
+        stepperInputEl.value = count;
+      }
 
-    if (count === stepperMin) {
-      stepperBtnMinusEl.classList.add("stepper__btn--disabled");
-    }
+      if (count === stepperMin) {
+        stepperBtnMinusEl.classList.add("stepper__btn--disabled");
+      }
+    });
   });
 }
